021f8a2c972df63bd108823dedfbe0cb98a096e8
techprolfaut <154212667+techprolfaut@users.noreply.github.com>
2024-10-15 06:28:58 -0700

    Add files via upload

---
+++ mainframe/src/main/java/com/proflaut/mf/MainframeApplication.java
@@ -0,0 +1,13 @@
+package com.proflaut.mf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainframeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainframeApplication.class, args);
+	}
+
+}
